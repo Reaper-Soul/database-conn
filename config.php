1ca82422bd53ffdb57eba89a99d58b0d5eab98f2
Navish Sharma <navishsharma2004@gmail.com>
2025-07-03 05:46:42 +0000

    stores the variables

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+  //variables
+  define('DB_HOST', 'i25ku.h.filess.io');
+  define('DB_USER', 'MariaMaria_awayglass');
+  define('DB_PASS', $_ENV['DB_PASS']);
+  define('DB_DATABASE', 'MariaMaria_awayglass');
+  define('DB_PORT', '3305');
+
+?>
